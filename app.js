991dab4374ab8b1e4ba341692a4d1731dae9ee84
ameyer-eng <72287053+ameyer-eng@users.noreply.github.com>
2023-02-18 17:29:49 -0600

    removed old files and routes

--- app.js
+++ app.js
@@ -11,10 +11,7 @@ var logger = require('morgan');
 const app = express();
 
 
-const indexRouter = require('./routes/index');
-
 const weatherRouter = require('./routes/weather');
-const images = require('./routes/images');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,9 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/weather', weatherRouter);
-app.use('/images', images);
+app.use('/', weatherRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- todo.md
+++ todo.md
@@ -1,4 +1,13 @@
 # todo
 
-1) Make it so when an image is clicked it goes to another page with a larger view
-2) Make <div> continers for the images
+1) Make a "day" page that gives hourly data.....
+
+2) Clean up the code in the weather_api.js
+
+3) Make it such that a zip code can be manually entered
+
+4) Make it such that it gets the weather code for the day and writes to screen
+
+5) Change the weather code text to icons
+
+
